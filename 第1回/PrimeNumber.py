5eab0d36719be2f5fd058dda6dddfc04139b0e5e
rkhrsm <rikuhiro0729@gmail.com>
2020-09-02 22:14:12 +0900

    no message

--- 第1回/PrimeNumber.py
+++ 第1回/PrimeNumber.py
@@ -1,5 +1,10 @@
 prime_list = []
-
-# TODO
-
+t=2
+while len(prime_list)<50:
+    for i in prime_list:
+        if t%i==0:
+            break
+    else:
+        prime_list.append(t)
+    t+=1
 print(prime_list)
