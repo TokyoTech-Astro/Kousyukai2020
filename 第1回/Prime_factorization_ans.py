907230def392c3a79d13c52d83d9b9f14a1de3a8
rkhrsm <rikuhiro0729@gmail.com>
2020-09-02 23:19:19 +0900

    no message

--- 第1回/Prime_factorization_ans.py
+++ 第1回/Prime_factorization_ans.py
@@ -1,13 +1,19 @@
+from math import sqrt
 n = int(input("n = "))
 prime_factor = []
 
-i = 2
-while i <= n:
-    if n % i:
-        i += 1
+sqrt_n = sqrt(n) + 1
+t=2
+while t<= n:
+    if n % t:
+        t += 1
+        if sqrt_n < t:
+            prime_factor.append(n)
+            break
     else:
-        n //= i
-        prime_factor.append(i)
+        n //= t
+        prime_factor.append(t)
+
 
 print("  = ", end = "")
 print(*prime_factor, sep=" x ")
